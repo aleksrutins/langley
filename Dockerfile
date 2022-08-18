bec5126b53c6e79b2be85a8aa49104988c53f310
Aleks Rutins <aleks@rutins.com>
2022-08-18 18:42:28 -0400

    Rewrite in Go

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM denoland/deno:alpine-1.20.1
-
-WORKDIR /app
-
-USER deno
-
-ADD . /app/
-
-CMD ["deno", "run", "--allow-net", "--allow-env", "index.ts"]
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+var ctx = context.Background()
+
+func main() {
+	_ = godotenv.Load()
+	redisOpts, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		println("Error parsing Redis URL")
+		return
+	}
+	rdb := redis.NewClient(redisOpts)
+
+	app := fiber.New()
+
+	app.Get("/:user/:repo", func(c *fiber.Ctx) error {
+		user := c.Params("user")
+		repo := c.Params("repo")
+		entry, err := rdb.Get(ctx, user).Result()
+		if err != nil {
+			res, err := http.Get(fmt.Sprintf("https://api.github.com/repos/%s/%s/languages", user, repo))
+			if err == nil {
+				bytes, err := ioutil.ReadAll(res.Body)
+				if err == nil {
+					entry = string(bytes)
+					expiration, err := time.ParseDuration("24h")
+					if err != nil {
+						expiration = time.Duration(0)
+					}
+					rdb.Set(ctx, fmt.Sprintf("%s/%s", user, repo), entry, expiration)
+				}
+			}
+		}
+		if err != nil {
+			return c.SendString(entry)
+		} else {
+			return errors.New("error fetching data")
+		}
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	app.Listen("0.0.0.0:" + port)
+}
