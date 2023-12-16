f35df9130fda1334d0c646a6de2d48c1876fd7ab
Aleks Rūtiņš <aleks@rutins.com>
2023-12-16 17:30:59 -0500

    error logs?:tm:

--- src/index.tsx
+++ src/index.tsx
@@ -8,21 +8,23 @@ type Bindings = {
 const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/:owner/:repo', async c => {
-  const kv = c.env.LANGLEY_CACHE
-      , owner = c.req.param('owner'), repo = c.req.param('repo')
-      , cachedLangs = await kv.get(`langs:${owner}/${repo}`);
-  
-  let langs: Record<string, number> | null = cachedLangs && JSON.parse(cachedLangs);
+  try {
+    const kv = c.env.LANGLEY_CACHE
+        , owner = c.req.param('owner'), repo = c.req.param('repo')
+        , cachedLangs = await kv.get(`langs:${owner}/${repo}`);
+    
+    let langs: Record<string, number> | null = cachedLangs && JSON.parse(cachedLangs);
 
-  if(!langs) {
-    langs = await (await fetch(`https://api.github.com/repos/${owner}/${repo}/languages`)).json() as Record<string, number>
-  }
+    if(!langs) {
+      langs = await (await fetch(`https://api.github.com/repos/${owner}/${repo}/languages`)).json() as Record<string, number>
+    }
 
-  return c.json(await Promise.all(Object.keys(langs).map(async lang => ({
-    language: lang,
-    amount: langs?.[lang],
-    color: await getLanguageColor(kv, lang)
-  }))))
+    return c.json(await Promise.all(Object.keys(langs).map(async lang => ({
+      language: lang,
+      amount: langs?.[lang],
+      color: await getLanguageColor(kv, lang)
+    }))))
+  } catch(e: any) { console.log(JSON.stringify(e)) }
 })
 
 export default app
